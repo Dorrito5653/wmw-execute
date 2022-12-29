648ab423066908440b5226c5b7f49d73f0a9521c
Dorrito5653 <112990492+Dorrito5653@users.noreply.github.com>
2022-12-28 22:45:03 -0500

    Removed app directory

--- app/src/scripts/login.js
+++
@@ -1,8 +0,0 @@
-async function login() {
-    
-}
-
-async function register() {
-
-}
-
--- app/src/scripts/sidebar.js
+++
@@ -1,9 +0,0 @@
-function openNav(){
-    document.getElementById('sidebar').style.transform = 'translateX(0)';
-    document.querySelector('.sidebar-openbtn').stype.display = 'none';
-}
-
-function closeNav(){
-    document.getElementById('sidebar').style.transform = "translateX(-200px)";
-    document.querySelector('.sidebar-openbtn').style.display = "initial";
-}
