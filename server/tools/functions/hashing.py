62a851340a7540a2cdc3a16193042173202dab28
username121546434 <gokuleatharv06@gmail.com>
2023-01-01 19:08:41 -0500

    added a way to compare hashed strings and non hashed strings
    Co-authored-by: Dorrito5653 <Dorrito5653@users.noreply.github.com>

--- server/tools/functions/hashing.py
+++ server/tools/functions/hashing.py
@@ -2,13 +2,15 @@ import hashlib
 import os
 import random
 import string
+import base64
+import secrets
 
 salt = os.urandom(30)
 
 
 def hash_string(string: str):
     """Hash the string and the salt using the SHA-256 algorithm"""
-    hashed_string = hashlib.sha256((string.encode() + salt)).hexdigest()
+    hashed_string = f"{base64.encodebytes(b'sha256').decode()}${hashlib.sha256(string.encode()).hexdigest()}${base64.encodebytes(salt).decode()}"
     return hashed_string
 
 
@@ -18,3 +20,13 @@ def gen_token():
     token = hash_string(''.join(random.choice(characters) for i in range(14)))
 
     return token
+
+
+def compare_hash_and_string(string: str, hash: str):
+    hash_algorithm, hashed_string, salt = hash.split('$')
+
+    hash_algorithm = base64.decodebytes(hash_algorithm.encode()).decode()
+    salt = base64.decodebytes(salt.encode()).decode()
+
+    if hash_algorithm == 'sha256':
+        return hashlib.sha256(string.encode()).hexdigest() == hashed_string
