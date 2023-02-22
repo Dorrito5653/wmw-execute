76d5d6f0efee79a0a3d3ff5dd669835ccac86867
Dorrito5653 <dorritoshah1543@gmail.com>
2023-02-21 21:18:06 -0500

    even more merging

--- game/renderer/scripts/index.ts
+++ game/renderer/scripts/index.ts
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-console.log('hello world');
-=======
 var countryNameElement = document.getElementById('country-name')
 console.log(countryNameElement)
 
@@ -8,4 +5,3 @@ console.log(countryNameElement)
 countryNameElement.oninput = (ev) => {
     
 }
->>>>>>> 9c87020eb3af61d9273deab4b582d2051cc7dbd7
