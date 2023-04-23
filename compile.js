b12e9b6f30ed0d303a4c3d2e6478996803ca717d
Dorrito5653 <dorritoshah1543@gmail.com>
2023-04-23 17:15:45 -0400

    edited stuff

--- compile.js
+++ compile.js
@@ -1,9 +1,12 @@
 console.log("Running compilation script");
-import * as chokidar from 'chokidar' 
+import * as chokidar from 'chokidar';
 import { exec } from 'child_process';
 
 // Directories to watch
-const directoriesToWatch = ['./game', './server']
+const directoriesToWatch = ['./game', './server', './game/renderer'];
+
+// Store a dictionary to keep track of the last compilation timestamp for each file
+const lastCompilationTimestamps = {};
 
 for (let i = 0; i < directoriesToWatch.length; i++) {
   const directoryToWatch = directoriesToWatch[i];
@@ -11,20 +14,49 @@ for (let i = 0; i < directoriesToWatch.length; i++) {
   console.log(`Watching directory '${directoryToWatch}'`);
 }
 
-function watchDir (dir) {
-  chokidar.watch(`${dir}/**/*.ts`, { ignored: /node_modules/ }).on('change', (path) => {
-    console.log(`File changed: ${path}`);
-    // Execute tsc -w command with the changed file as an argument
-    exec(`tsc -w ${path}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-      }
-      if (stderr) {
-        console.error(`Stderr: ${stderr}`);
-      }
-      if (stdout) {
-        console.log(`Stdout: ${stdout}`);
+function watchDir(dir) {
+  chokidar
+    .watch(`${dir}/**/*.ts`, { ignored: /node_modules/ })
+    .on('change', (path) => {
+      console.log(`File changed: ${path}`);
+      // Get the current timestamp
+      const currentTimestamp = Date.now();
+      // Get the last compilation timestamp for the file, if it exists
+      const lastTimestamp = lastCompilationTimestamps[path] || 0;
+      // Calculate the time elapsed since the last compilation
+      const timeElapsed = currentTimestamp - lastTimestamp;
+
+      // Use a debounce delay of 1 second (1000 milliseconds)
+      const debounceDelay = 1000;
+
+      // If the time elapsed since the last compilation is greater than the debounce delay
+      if (timeElapsed > debounceDelay) {
+        // Update the last compilation timestamp for the file
+        lastCompilationTimestamps[path] = currentTimestamp;
+
+        // Execute tsc -w command with the changed file as an argument
+        exec(`tsc ./game/renderer/scripts/actors/types.d.ts`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error: ${error.message}`);
+          }
+          if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+          }
+          if (stdout) {
+            console.log(`Stdout: ${stdout}`);
+          }
+        });
+        exec(`tsc ${path}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error: ${error.message}`);
+          }
+          if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+          }
+          if (stdout) {
+            console.log(`Stdout: ${stdout}`);
+          }
+        });
       }
     });
-  });
 }
