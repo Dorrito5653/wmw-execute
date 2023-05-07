dbf235695d692e1155fea1b2c58ef2d3d080579a
username121546434 <gokuleatharv06@gmail.com>
2023-05-07 18:32:12 -0400

    Improved the map generation function using octaves

--- game/renderer/scripts/map-generator.ts
+++ game/renderer/scripts/map-generator.ts
@@ -13,24 +13,44 @@ export enum TileType {
 
 const ELEVATION_FREQUENCY = 0.0045; // how often elevation changes
 
+function getNoise(x: number, y: number, octaves: number = 7): number {
+    let amplitudes: number[] = []
+    let octave_coormultipliers: number[] = []
+
+    for (let i = 1; i < octaves; i++) {
+        octave_coormultipliers.push(i)
+        amplitudes.push(1 / i)
+    }
+
+    let noise = 0
+
+    octave_coormultipliers.forEach((coormultiplier, idx) => {
+        let amplitude = amplitudes[idx]
+        noise += noise2D(x * coormultiplier * ELEVATION_FREQUENCY, y * coormultiplier * ELEVATION_FREQUENCY) * amplitude
+    })
+    noise = Math.tanh(noise)
+
+    return noise
+}
+
 export function generateMap(width: number, height: number) {
     let map: number[][] = [];
 
     for (let x = 0; x < width; x++) {
         let row: number[] = [];
         for (let y = 0; y < height; y++) {
-            const elevation = noise2D(x * ELEVATION_FREQUENCY, y * ELEVATION_FREQUENCY);
+            const elevation = getNoise(x, y)
 
             let tileType = TileType.deepWater;
-            if (elevation < -0.2) {
+            if (elevation < -0.1) {
                 tileType = TileType.deepWater
             } else if (elevation < 0) {
                 tileType = TileType.shallowWater;
             } else if (elevation < 0.1) {
                 tileType = TileType.beaches;
-            } else if (elevation < 0.5) {
+            } else if (elevation < 0.55) {
                 tileType = TileType.plains;
-            } else if (elevation < 0.8) {
+            } else if (elevation < 0.75) {
                 tileType = TileType.hills;
             } else {
                 tileType = TileType.mountains;
